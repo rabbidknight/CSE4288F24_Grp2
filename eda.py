59ddc621947f54f439cdcffe296c3afa44f95279
Duru Baştunalı <durubastunali@gmail.com>
2024-12-15 23:27:30 +0300

    EDA update

--- eda.py
+++ eda.py
@@ -29,6 +29,11 @@ total_annotations = train_total + val_total
 all_crosswalks_per_image = train_crosswalks_per_image + val_crosswalks_per_image
 images_with_crosswalks = len([count for count in all_crosswalks_per_image if count > 0])
 
+count_per_image = 0
+max_count = 0
+min_count = 10
+
+
 # Print the results
 print("Crosswalk Statistics:")
 print(f"Total number of crosswalk annotations: {total_annotations}")
@@ -36,3 +41,13 @@ print(f"Total number of images with crosswalks: {images_with_crosswalks}")
 print("Number of crosswalks per image:")
 for i, count in enumerate(all_crosswalks_per_image, start=1):
     print(f"Image {i}: {count} crosswalk(s)")
+    count_per_image += count
+    if count < min_count:
+        min_count = count
+    if count > max_count:
+        max_count = count
+
+
+print("Average crosswalk count per image is:", count_per_image / images_with_crosswalks)
+print("Maximum crosswalk count per image is:", max_count)
+print("Minimum crosswalk count per image is:", min_count)
